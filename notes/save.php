5457debe90ac6b43ce874e7f128002a7482f088e
Don-Victor-123 <109880765+Don-Victor-123@users.noreply.github.com>
2025-07-17 21:29:17 -0600

    Add default admin user to SQL schema

--- notes/save.php
+++ notes/save.php
@@ -7,16 +7,17 @@ $user_id = $_SESSION['user_id'];
 $data = json_decode(file_get_contents('php://input'), true);
 $group   = intval($data['group_id']);
 $content = $data['content'];
+$status  = $data['status'] ?? 'Pendiente';
 $id      = isset($data['id']) ? intval($data['id']) : 0;
 
 if ($id>0) {
   // UPDATE
-  $stmt = $pdo->prepare("UPDATE notes SET content=?, updated_at=NOW() WHERE id=? AND author_id=?");
-  $stmt->execute([$content, $id, $user_id]);
+  $stmt = $pdo->prepare("UPDATE notes SET content=?, status=?, updated_at=NOW() WHERE id=? AND author_id=?");
+  $stmt->execute([$content, $status, $id, $user_id]);
 } else {
   // INSERT
-  $stmt = $pdo->prepare("INSERT INTO notes (group_id,author_id,content) VALUES (?,?,?)");
-  $stmt->execute([$group, $user_id, $content]);
+  $stmt = $pdo->prepare("INSERT INTO notes (group_id,author_id,content,status) VALUES (?,?,?,?)");
+  $stmt->execute([$group, $user_id, $content, $status]);
   $id = $pdo->lastInsertId();
 }
 
